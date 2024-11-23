7a839d50ee05c6046968174e01df1a3a46860c08
Desmond Obisi <desmondcutest@yahoo.com>
2024-11-23 18:09:12 +0100

    chore: test dev

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://athene-ai.0xanon.workers.dev/api';
-export const API_URL = 'http://localhost:8787/api';
+export const API_URL = 'https://athene-ai.0xanon.workers.dev/api';
+// export const API_URL = 'http://localhost:8787/api';
